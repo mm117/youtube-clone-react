2a72babb77a4964043dd0136c1da89597bc9c7fb
mm117 <mukeshmishra117@gmail.com>
2020-05-10 17:22:52 +0530

    styles changes

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -3,17 +3,18 @@ import { Grid, Paper, Typography } from "@material-ui/core";
 
 const VideoItem = ({ video, onVideoSelect }) => {
   return (
-    <Grid item >
-      <Paper style={{ display: "flex", alignItem: "center", cursor:'pointer' }} onClick={() => onVideoSelect(video) }>
-        <img
-          style={{ marginRight: "20px" }}
-          alt="thumbnail"
-          src={video.snippet.thumbnails.medium.url}
-        />
-        <Typography variant="subtitle1">
-          <b>{video.snippet.title}</b>
-        </Typography>
+    <Grid item xs={12}>
+      <Paper
+        style={{ display: "flex", alignItem: "center", cursor: "pointer" }}
+        onClick={() => onVideoSelect(video)}
+      >
+        <img alt="thumbnail" src={video.snippet.thumbnails.medium.url} style={{width: '100%', height:'100%'}} />
+       
       </Paper>
+      <Paper  style={{ display: "flex", alignItem: "center", cursor: "pointer" }}   
+         onClick={() => onVideoSelect(video)}>
+            <Typography variant="subtitle1"><b>{video.snippet.title} </b></Typography>
+        </Paper>
     </Grid>
   );
 };
